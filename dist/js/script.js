ddc9a74d0eee60153a54c262fa38166217daf517
Andrii <2andrew.demchenko@gmail.com>
2022-12-12 00:21:16 +0200

    5th commit full responsive + switchTheme +lang+-

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,6 +1,11 @@
 const toggleSwitch = document.querySelector('.theme-switch input[type="checkbox"]');
 const currentTheme = localStorage.getItem('theme');
+const replaceWord = document.querySelector('.theme_mode');
 
+const select = document.querySelector('select');
+const allLang = ['en', 'ua'];
+
+//theme-switcher
 if (currentTheme) {
     document.documentElement.setAttribute('data-theme', currentTheme);
   
@@ -14,9 +19,51 @@ function switchTheme(e) {
         document.documentElement.setAttribute('data-theme', 'dark');
         localStorage.setItem('theme', 'dark');
     }
-    else {        document.documentElement.setAttribute('data-theme', 'light');
-          localStorage.setItem('theme', 'light');
+    else { 
+        document.documentElement.setAttribute('data-theme', 'light');
+        localStorage.setItem('theme', 'light');
     }    
 }
 
 toggleSwitch.addEventListener('change', switchTheme, false);
+
+// word-switcher
+toggleSwitch.addEventListener('change', function(){
+    const currentTheme = localStorage.getItem('theme');
+    if (currentTheme === 'light'){
+        replaceWord.innerText = 'Enable Dark Mode!';
+    } else{
+        replaceWord.innerText = 'Enable Light Mode!';
+    }
+});
+
+// lang-switcher
+select.addEventListener('change', changeURLLanguage);
+
+function changeURLLanguage() {
+    let lang = select.value;
+    location.href = window.location.pathname + '#' + lang;
+    location.reload();
+}
+
+function changeLanguage() {
+    let hash = window.location.hash;
+    hash = hash.substr(1);
+    console.log(hash);
+    if (!allLang.includes(hash)) {
+        location.href = window.location.pathname + '#en';
+        location.reload();
+    }
+    select.value = hash;
+    document.querySelector('title').innerHTML = langArr['title'][hash];
+    // document.querySelector('.lng-chip').innerHTML = langArr['chip'][hash];
+    for (let key in langArr) {
+        let elem = document.querySelector('.lng-' + key);
+        if (elem) {
+            elem.innerHTML = langArr[key][hash];
+        }
+
+    }
+}
+
+changeLanguage();
---
+++ src/js/lang.js
@@ -0,0 +1,117 @@
+const langArr = {
+    'title': {
+        'en': 'Portfolio',
+        'ua': 'Портфоліо',
+    },
+    'nav1': {
+        'en': 'tech skils', 
+        'ua': 'стек',
+    },
+    'nav2': {
+        'en': 'education',
+        'ua': 'освіта',
+    },
+    'nav3': {
+        'en': 'projects',
+        'ua': 'проекти',
+    },
+    'nav4': {
+        'en': 'contact me',
+        'ua': 'контакти',
+    },
+    'h1': {
+        'en': 'Hello 👋,<br> My name is Andrii.<br> I`m studyng to become<br> a web developer',
+        'ua': 'Привіт 👋,<br> мене звати Андрій.<br> Я вчуся на<br> Веб-розробника',
+    },
+    'stackTitle': {
+        'en': 'My Tech stack',
+        'ua': 'Мій стек',
+    },
+    'stackSubTitle': {
+        'en': 'Technologies I’ve been working with recently',
+        'ua': 'Програми з якими працюю',
+    },
+    'stackSubTitle2': {
+        'en': 'Studing now',
+        'ua': 'Зараз вивчаю',
+    },
+    'educationTitle': {
+        'en': 'Education',
+        'ua': 'Освіта',
+    },
+    'educationSubTitle': {
+        'en': 'Universities and courses that I completed',
+        'ua': 'Університети та курси',
+    },
+    'ssu': {
+        'en': 'Sumy State University',
+        'ua': 'Сумський державний університет',
+    },
+    'elit': {
+        'en': 'Faculty of Electronics and Information Technologies',
+        'ua': 'Факультет електроніки та інформаційних технологій',
+    },
+    'bachelor': {
+        'en': 'bachelor diploma | Sumy (2017-2021)',
+        'ua': 'диплом бакалавра | Суми (2017-2021)',
+    },
+    'snau': {
+        'en': 'Sumy National Agrarian University',
+        'ua': 'Сумський національний аграрний університет',
+    },
+    'marketing': {
+        'en': 'Faculty of Economics and Management | Marketing',
+        'ua': 'Факультет економіки та менеджменту | Маркетинг',
+    },
+    'soon1': {
+        'en': 'comming soon...',
+        'ua': 'незабаром...',
+    },
+    'udemy': {
+        'en': 'Udemy | online learning marketplace',
+        'ua': 'Udemy | онлайн-ринок навчання',
+    },
+    'webDev': {
+        'en': 'Web developer 2022 | Frontend',
+        'ua': 'Веб-розробник 2022 | Frontend',
+    },
+    'soon2': {
+        'en': 'comming soon...',
+        'ua': 'незабаром...',
+    },
+    'projects': {
+        'en': 'Projects',
+        'ua': 'Проекти',
+    },
+    'projectsSub': {
+        'en': 'My finished projects that I worked on',
+        'ua': 'Мої готові проекти, над якими я працював',
+    },
+    'soon2': {
+        'en': 'comming soon...',
+        'ua': 'незабаром...',
+    },
+
+
+
+
+
+
+    'nav5': {
+        'en': 'tech skils', 
+        'ua': 'стек',
+    },
+    'nav6': {
+        'en': 'education',
+        'ua': 'освіта',
+    },
+    'nav7': {
+        'en': 'projects',
+        'ua': 'проекти',
+    },
+    'nav8': {
+        'en': 'contact me',
+        'ua': 'контакти',
+    },
+    
+}
