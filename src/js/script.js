48eb6f5df1b3b9f4f696dac66bb601c940d55f13
A <2andrew.demchenko@gmail.com>
2022-12-14 21:11:26 +0200

    8th commit add 3 projects

--- src/js/script.js
+++ src/js/script.js
@@ -76,3 +76,17 @@ function myFunction() {
         x.style.display = "block";
     }
 } 
+
+$(document).ready(function(){
+    $(window).scroll(function(){
+        if($(this).scrollTop() > 1000){
+            $('.pageup').fadeIn();   
+        } else {
+            $('.pageup').fadeOut();   
+        };
+    });
+    $("a[href='#up']").click(function() {
+        $("html, body").animate({ scrollTop: 0 }, "slow");
+        return false;
+      });
+});   
